b656c6891885673227a4d38e499a5fc0356ab73c
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-09-30 11:27:44 +0200

    add post characters endpoint
    add endpoint to add new characters to the characters.json using fs nodejs module

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const express = require('express');
 const fs = require('fs').promises; // es de node, no es una librería
 const app = express()
 
+app.use(express.json()); //sin estos req.body es undefined
 app.get('/characters', async(req, res) => {
     try {
-        const characters = await fs.readFile('characters.json', {
-            encoding: 'utf8'
-        })
+        const characters = await fs.readFile('characters.json', 'utf8')
         res.send(JSON.parse(characters))
     } catch (error) {
         console.error(error)
-        res.status(500).send({ message: 'Sorry, there was a problem trying to get the characters' })
+        res.status(500).send({
+            message: 'Sorry, there was a problem trying to get the characters'
+        })
     }
 })
-app.get('/about', (req, res) => res.send('esto es about'));
+
+app.post('/characters', (req, res) => {
+    fs.readFile('characters.json', 'utf-8')
+        .then(rawValues => JSON.parse(rawValues))
+        .then(values => {
+            const characters = values.results;
+            characters.push(req.body);
+            return fs.writeFile('characters.json', JSON.stringify(values), null, 4)
+
+        })
+        .then(() => res.status(201).send({ message: 'Character successfully created' }))
+})
+
 app.listen(3000, () => console.log('server running on port 3000'))
